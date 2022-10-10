170ff7e42237f60df43664e47a1846dbc5978b0b
Jasmyne Jean-Remy <jasmynej14@gmail.com>
2022-10-10 13:41:33 -0400

    pagination

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,9 +3,9 @@ function Movie({movie}){
 
     return (
         <div className="movie-card">
-            <i class="fa-solid fa-film"></i>
-            <h3 className="movie-card-title">{movie.movieTitle}</h3>
-            <p className="movie-card-description">{movie.movieDescription}</p>
+            <i className="fa-solid fa-film"></i>
+            <h3 className="movie-card-title">{movie.title}</h3>
+            <p className="movie-card-description">{movie.description}</p>
         </div>
     )
 }
--- src/pages/AllMovies.js
+++ src/pages/AllMovies.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
 import {useState,useEffect} from "react"
 import Movie from '../components/Movie'
+import ReactPaginate from 'react-paginate';
 import './sass/movie_pages.scss'
 function AllMovies(){
     const [movies,setMovies] = useState([])
+    const [pageCount, setPageCount] = useState(0)
+    const [itemOffset, setItemOffset] = useState(0);
+    const [currentMovies,setCurrentMovies] = useState([])
 
+    const moviesPerPage = 36;
     const getMovies = () => {
         axios.get("http://localhost:8080/api/movies")
         .then(function(response){
@@ -15,22 +20,56 @@ function AllMovies(){
 
     useEffect(() => {
         getMovies()
+
     },[])
-    return (
-        <div>
-            <h1>All Movies ({movies.length})</h1>
-            <div className="movie-grid">
-                {
-                    movies.map((movie) => {
-                        return (
-                            <Movie movie={movie} key={movie.id} />
-                        )
-                    })
-                }
+
+    useEffect(() => {
+        const endOffset = itemOffset + moviesPerPage;
+        console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+        setCurrentMovies(movies.slice(itemOffset,endOffset))
+        setPageCount(Math.ceil(movies.length / moviesPerPage))
+    },[itemOffset,moviesPerPage])
+
+    const changePage = (event) => {
+        const newOffset = (event.selected * moviesPerPage) % movies.length;
+        console.log(`Moving to page ${event.selected} which is offset ${newOffset}`)
+        setItemOffset(newOffset);
+    }
+    if(currentMovies.length !== 0){
+        return (
+            <div>
+                <h1>All Movies ({movies.length})</h1>
+                <div className="movie-grid">
+                    {
+                        currentMovies.map((movie) => {
+                            return (
+                                <Movie movie={movie} key={movie.id} />
+                            )
+                        })
+                    }
+                </div>
+                <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={changePage}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+                pageLinkClassName="page-link"
+                activeLinkClassName='active'
+                containerClassName='page-container'
+                previousClassName='page-link'
+                nextClassName='page-link '
+            />
+                
             </div>
-            
-        </div>
-    )
+        )
+    }
+    else {
+        return "loading movies"
+    }
+    
 }
 
 export default AllMovies;
